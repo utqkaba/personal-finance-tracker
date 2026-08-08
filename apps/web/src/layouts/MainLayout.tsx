7481196d69f68e57c7b2b51d2939bfe6874f8cb0
utqkaba <utqkaba@gmail.com>
2026-08-09 01:58:52 +0300

    fix: prevent dashboard layout overflow

--- apps/web/src/layouts/MainLayout.tsx
+++ apps/web/src/layouts/MainLayout.tsx
@@ -5,10 +5,10 @@ import Sidebar from "../components/layout/Sidebar";
 
 function MainLayout() {
   return (
-    <div className="flex h-screen">
+    <div className="flex min-h-screen">
       <Sidebar />
 
-      <div className="flex flex-1 flex-col">
+      <div className="flex min-w-0 flex-1 flex-col">
         <Header />
 
         <main className="flex-1 bg-stone-200 p-6 border-t-4 border-l-4 border-double border-stone-300 rounded-lg">
