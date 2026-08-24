a015419326102e9488d79782f1f51708bf5e5100
utqkaba <utqkaba@gmail.com>
2026-08-24 19:19:59 +0300

    feat: the user section of the investment page has been improved

--- apps/web/src/pages/InvestmentPage.tsx
+++ apps/web/src/pages/InvestmentPage.tsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+
+import InvestmentFormModal from "../components/investment/InvestmentFormModal";
+
 function InvestmentPage() {
-  return <div>InvestmentPage</div>;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <section className="flex items-center justify-between rounded-xl bg-stone-100 p-6 shadow-lg">
+        <button
+          type="button"
+          onClick={() => setIsModalOpen(true)}
+          className="cursor-pointer rounded-2xl bg-linear-to-r from-blue-100 to-blue-700 px-7 py-2 font-extralight text-white transition-transform duration-500 hover:scale-105"
+        >
+          + Add Investment
+        </button>
+
+        <div>
+          <h1 className="text-xl font-medium text-stone-900">Investments</h1>
+
+          <p className="text-sm font-extralight italic text-stone-500">
+            Track your investments and their current value.
+          </p>
+        </div>
+      </section>
+      <InvestmentFormModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
+    </>
+  );
 }
 
 export default InvestmentPage;
